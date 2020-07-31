3a46276af0d5e783a4d120781ef6e3f868ae1e4e
sugimo-ne <kein.sugimoto@gmail.com>
2020-07-31 12:34:58 +0900

    install css-loader & style-loader

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [{ loader: "style-loader" },{ loader: "css-loader" },],
+      },
+    ],
+  },
+};
